22a6039064ccdd285f9206f7faf0a37aaf9abcde
Blaine <blaineturley@gmail.com>
2020-11-13 21:40:58 -0500

    fixed typo

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///demilich'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.debug = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'noneofyourbeeswax"\')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'noneofyourbeeswax')
 
 toolbar = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False

f3d657163a5947ca3b406e24c1352dca5ef87882
Blaine <blaineturley@gmail.com>
2020-12-22 16:28:18 -0500

    ...

--- static/app.js
+++ static/app.js
@@ -1221,5 +1221,5 @@ $(".item-drop").on("click", async function(event) {
 /* ------------------------REPOSITION BACKGROUND ON MOBILE------------------------ */
 
 $( document ).ready(function() {
-    $('body').addClass("backgroundmobile")
+    $('body').css("background-position", "center");
 });
